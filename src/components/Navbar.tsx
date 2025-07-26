62d9e1372406a07cb765743649e20cfcf3f7061f
Rowan Murray <rowanomurray@gmail.com>
2025-07-26 22:08:33 +0100

    style: fix responsiveness in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 export const Navbar = () => {
   return (
     <nav className="max-w-3xl mx-auto mb-4 px-4 text-white">
-      <div className="flex justify-between items-center gap-4 min-h-20 bg-grey rounded-b-3xl p-4">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 min-h-20 bg-grey rounded-b-3xl p-4">
         <h1 className="text-xl sm:text-2xl font-bold text-center">
           <Link href="/" className="hover:underline">
             Table Tennis Drills
